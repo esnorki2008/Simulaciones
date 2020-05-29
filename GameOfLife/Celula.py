fc69326860266726a97f8126b61b3a0f0947ae6f
esnorki2008 <49577095+esnorki2008@users.noreply.github.com>
2020-05-29 09:44:08 -0600

    0
    0

--- GameOfLife/Celula.py
+++ GameOfLife/Celula.py
@@ -1,42 +1,91 @@
 class Celula:
     Viva=False
+    Turno=False
     PosActual=(0,0)
-    def __init__(self):
+    def __init__(self,PosActual):
         self.PosActual=PosActual
 
-    def Interactuar(self,PosActual):
-        Vecinos+=Izquierda
-        Vecinos+=Derecha
-        Vecinos+=Arriba
-        Vecinos+=Abajo
+    def Interactuar(self,Lista,Limite):
+        if (not self.Turno) and True :
+            return
 
-        Vecinos+=IzquierdaArriba
-        Vecinos+=DerechaAbajo
-        Vecinos+=IzquierdaArriba
-        Vecinos+=IzquierdaAbajo
+        Vecinos=0
+        Vecinos+=self.Izquierda(self.PosActual,Lista,Limite)
+        Vecinos+=self.Derecha(self.PosActual,Lista,Limite)
+        Vecinos+=self.Arriba(self.PosActual,Lista,Limite)
+        Vecinos+=self.Abajo(self.PosActual,Lista,Limite)
 
-        if Vecinos == 3
-            Viva== True
+        Vecinos+=self.IzquierdaArriba(self.PosActual,Lista,Limite)
+        Vecinos+=self.DerechaAbajo(self.PosActual,Lista,Limite)
+        Vecinos+=self.IzquierdaArriba(self.PosActual,Lista,Limite)
+        Vecinos+=self.IzquierdaAbajo(self.PosActual,Lista,Limite)
+
+        if Vecinos == 3:
+            Viva= True
+        elif Vecinos ==2 and (not Viva) :
+            Viva=True
+        if Vecinos > 3 or Vecinos < 2:
+            Viva=False
         
-
-    def Izquierda(self,PosActual):
-        pass
-    def Derecha(self,PosActual):
-        pass
-    def Arriba(self,PosActual):
-        pass
-    def Abajo(self,PosActual):
-        pass
+        self.Turno =False
 
 
-    def IzquierdaArriba(self,PosActual):
-        pass
+    def LLamarVecina(self,PosVecina,Lista,Limite):
+            if not self.Viva :
+                return 0
+            Obj = Lista.get(PosVecina)            
+            Obj.Interactuar(Lista,Limite)
+            if Obj.Viva :
+                return 1
+            else :
+                return 0
 
-    def DerechaArriba(self,PosActual):
-        pass
+
+    def Izquierda(self,PosActual,Lista,Limite):
+        if self.PosActual[0]>1:      
+            return self.LLamarVecina((self.PosActual[0]-1,self.PosActual[1]),Lista,Limite)
+        else:
+            return 0
+
+    def Derecha(self,PosActual,Lista,Limite):
+        if self.PosActual[0]<Limite[0]:      
+            return self.LLamarVecina((self.PosActual[0]+1,self.PosActual[1]),Lista,Limite)
+        else:
+            return 0
+
+    def Arriba(self,PosActual,Lista,Limite):
+        if self.PosActual[1]<Limite[1]:      
+            return self.LLamarVecina((self.PosActual[0],self.PosActual[1]+1),Lista,Limite)
+        else:
+            return 0
+
+    def Abajo(self,PosActual,Lista,Limite):
+        if self.PosActual[1]>1:      
+            return self.LLamarVecina((self.PosActual[0],self.PosActual[1]-1),Lista,Limite)
+        else:
+            return 0
+
+
+    def IzquierdaArriba(self,PosActual,Lista,Limite):
+        if self.PosActual[1]<Limite[1] and self.PosActual[0]>0:      
+            return self.LLamarVecina((self.PosActual[0]-1,self.PosActual[1]+1),Lista,Limite)
+        else:
+            return 0
+
+    def DerechaArriba(self,PosActual,Lista,Limite):
+        if self.PosActual[1]<Limite[1] and self.PosActual[0]<Limite[0]:      
+            return self.LLamarVecina((self.PosActual[0]+1,self.PosActual[1]+1),Lista,Limite)
+        else:
+            return 0
     
-    def IzquierdaAbajo(self,PosActual):
-        pass
+    def IzquierdaAbajo(self,PosActual,Lista,Limite):
+        if self.PosActual[1]>0 and self.PosActual[0]>0:      
+            return self.LLamarVecina((self.PosActual[0]-1,self.PosActual[1]-1),Lista,Limite)
+        else:
+            return 0
 
-    def DerechaAbajo(self,PosActual):
-        pass
+    def DerechaAbajo(self,PosActual,Lista,Limite):
+        if self.PosActual[1]>0 and self.PosActual[0]<Limite[0]:      
+            return self.LLamarVecina((self.PosActual[0]+1,self.PosActual[1]-1),Lista,Limite)
+        else:
+            return 0
--- GameOfLife/Main.py
+++ GameOfLife/Main.py
@@ -1,10 +1,24 @@
 import sys, pygame
+import Celula
 pygame.init()
-size = width, height = 320, 240
+size = width, height = 640, 480
 speed = [2, 2]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+
+Lista ={}
+for i in range(100):
+    for j in range(100):
+        CeluNueva = Celula.Celula((i,j))
+        Lista[i,j]=CeluNueva
+        
+Cel = Lista.get((50,50))
+Cel.Viva=True
+Cel = Lista.get((51,50))
+Cel.Viva=True
+Cel = Lista.get((52,50))
+Cel.Viva=True
 
 
 def IniciarJogo():
@@ -15,14 +29,15 @@ def IniciarJogo():
 
 
         pygame.time.wait(200)
-        for Herbi in ListaHerbivoro:            
-            screen.set_at((Herbi.PosActual[0]+135, Herbi.PosActual[1]+95), Herbi.Color())
-            Herbi.Mover()
 
-        for Planta in ListaPnlanta:            
-            screen.set_at((Planta.PosActual[0]+135, Planta.PosActual[1]+95), Planta.Color())
-            Planta.Expandir(ListaPnlanta,size)
-
+        for i in range(100):
+            for j in range(100):
+                Celu=Lista.get((i,j))
+                if Celu.Viva :
+                    Celu.Turno=True
+                    Celu.Interactuar(Lista,size)       
+                    screen.set_at(Celu.PosActual, (255,255,255))
+      
         pygame.display.update() 
        
         #pygame.draw.circle(screen, (255,255,255), (50,50), 0)
